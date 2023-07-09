01fb81cd9ad4a08b009d652154a09195f243324c
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-07-09 14:39:40 +0300

    add

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,21 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+  };
+  componentDidMount() {
+    ImageService.getImages('cat', 1);
+  }
+  onSubmit = query => {
+    console.log(query);
+    this.setState({ query });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.onSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
