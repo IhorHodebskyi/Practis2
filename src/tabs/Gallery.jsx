a5a762e78b899eb20539240e3c9ba38b951e2207
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-07-09 15:22:47 +0300

    commit 2

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -6,19 +6,55 @@ import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 export class Gallery extends Component {
   state = {
     query: '',
+    page: 1,
+    imagesList: [],
+    isLoading: false,
+    error: null,
   };
   componentDidMount() {
-    ImageService.getImages('cat', 1);
+    // ImageService.getImages('cat', 1);
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query) {
+      this.getImeges(query, page);
+    }
+  }
+
+  getImeges = async (query, page) => {
+    try {
+      this.setState({ isLoading: true });
+      const response = await ImageService.getImages(query, page);
+      console.log(response);
+      this.setState({ imagesList: response.photos });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setTimeout(() => this.setState({ isLoading: false }), 1000);
+    }
+  };
+
   onSubmit = query => {
     console.log(query);
     this.setState({ query });
   };
 
   render() {
+    const { imagesList, isLoading } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.onSubmit} />
+        <Grid>
+          {imagesList.map(({ id, alt, src: { small } }) => (
+            <GridItem key={id}>
+              <CardItem>
+                <img src={small} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {isLoading && <Text textAlign="center">Loading ... </Text>}
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
